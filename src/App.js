ef5cbe64b4103140462d20d1252e120302dd73e9
Kiran-pande-30 <kpande026@gmail.com>
2024-03-29 02:38:43 +0530

    fix: bug-search

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Favourite from './pages/Favourite';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element = {<Home/>} />
       <Route path='/favourite' element={<Favourite/>}/>
+      <Route path='/search' element={<SearchBar/>}/>
     </Routes>
   );
 }
