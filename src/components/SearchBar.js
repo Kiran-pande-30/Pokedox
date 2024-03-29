95f71cdfc381c1261f14c134a4ac7b767a4e193d
Kiran-pande-30 <kpande026@gmail.com>
2024-03-29 16:22:41 +0530

    Improve-bug-search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 import PokeCard from './PokeCard';
+import PokeGrid from './PokeGrid'
+
 
 export default function SearchBar() {
 
   const [input, setInput] = useState("");
   const [results, setResults] = useState([]);
+
+  const [img, setImg] = useState('');
+
+  const [type, setType] = useState([]);
+  var arr = [];
 
   const handleChange = (e)=>{
     const promises = [];
@@ -13,18 +20,25 @@ export default function SearchBar() {
       fetch(`https://pokeapi.co/api/v2/pokemon/${e.target.value}`).then(res=>res.json()).then(result => {
       promises.push(result);
 		    setResults(result);
-		    console.log(result);
-		})
+        setImg(`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${
+          result.id < 10
+            ? '00' + result.id
+            : result.id < 100
+            ? '0' + result.id
+            : +result.id
+        }.png`)
+        Object.values(result)[16].map((e)=>{
+          arr.push(e.type.name)
+          setType(arr);
+        })
+		      })
     }
   }
-
-  Object.values(results)[16].map((e)=>{
-    console.log(e.type.name);
-  })
 
   return (
     <div>
        <input onChange={handleChange} value={input}/>
+       <PokeCard name={results.name} type={type} img={img}/>
       <ul>
         {/* {
           Object.values(results).map((item) => {
